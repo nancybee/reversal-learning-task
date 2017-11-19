686c0fd9b95464fa75a52ec0d922d623b236b652
yazeedb <ybzadough@gmail.com>
2017-11-19 14:58:55 -0500

    edit conclusion text

--- js/Conclusion.js
+++ js/Conclusion.js
@@ -6,8 +6,8 @@
     <div className="conclusion">
         <h1>The experiment has concluded.</h1>
 
-        <h3>You pressed the blue circle { checkPluralCircle(timesChosenA) } </h3>
-        <h3>You pressed the orange circle { checkPluralCircle(timesChosenB) }</h3>
+        <h3>You chose the blue circle { checkPluralCircle(timesChosenA) } </h3>
+        <h3>You chose the orange circle { checkPluralCircle(timesChosenB) }</h3>
         <h3>You've earned { totalPoints } total points.</h3>
 
         <h1>Thank you for your participation, { initials }.</h1>
